3662630fa477780398f4e33316f4b82d5909875c
Riccardo2050 <104273564+RiccardoSC2050@users.noreply.github.com>
2026-05-19 18:26:15 +0200

    front-end header completato

--- web/primo progetto/app/includes/footer.php
+++
@@ -1,9 +0,0 @@
-    </main>
-    <footer class="site-footer">
-        <div class="container">
-            <small>Struttura software del progetto telefonia.</small>
-        </div>
-    </footer>
-    <script src="<?= e(appConfig()['base_url']) ?>js/app.js"></script>
-</body>
-</html>
--- web/primo progetto/app/views/contratto_dettaglio.php
+++
@@ -1,8 +0,0 @@
-<section>
-    <h1>Dettaglio contratto</h1>
-    <?php if (!$contratto): ?>
-        <p>Contratto non trovato.</p>
-    <?php else: ?>
-        <pre><?= e(print_r($contratto, true)) ?></pre>
-    <?php endif; ?>
-</section>
